cec7b6bbb0c0f2db23deabf7a26170f63f5b32f5
JBBDC <matan4bb@gmail.com>
2020-02-02 23:37:07 +0500

    refactor VoteService

--- src/main/java/com/task/poll/service/VoteService.java
+++ src/main/java/com/task/poll/service/VoteService.java
@@ -18,19 +18,10 @@ import static com.task.poll.util.DateTimeUtil.getStartIfNull;
 public class VoteService {
     private CrudVoteRepository repository;
     private CrudUserRepository userRepository;
-    private RestaurantService restaurantService;
 
-    public VoteService(CrudVoteRepository repository, CrudUserRepository userRepository, RestaurantService restaurantService) {
+    public VoteService(CrudVoteRepository repository, CrudUserRepository userRepository) {
         this.repository = repository;
         this.userRepository = userRepository;
-        this.restaurantService = restaurantService;
-    }
-
-    public List<Vote> getByUserBetweenDates(int userId, @Nullable LocalDate startDate, @Nullable LocalDate endDate) {
-        if (startDate == null && endDate == null) {
-            return repository.getAllByUser(userId);
-        }
-        return repository.getByUserBetweenDates(userId, getStartIfNull(startDate), getEndIfNull(endDate));
     }
 
     public List<Vote> getBetweenDates(@Nullable LocalDate startDate, @Nullable LocalDate endDate) {
@@ -52,20 +43,14 @@ public class VoteService {
         return repository.getByDateAndUserId(date, userId).orElse(null);
     }
 
-    public Vote save(Vote vote) {
-        return repository.save(vote);
-    }
-
     public Vote create(Restaurant restaurant, int userId){
-        Vote vote = new Vote();
-        vote.setRestaurant(restaurant);
-        vote.setUser(userRepository.getOne(userId));
-        return save(vote);
+        Vote vote = new Vote(userRepository.getOne(userId), restaurant);
+        return repository.save(vote);
     }
 
     public Vote update(Restaurant restaurant, int voteId, int userId){
         Vote vote = new Vote(userRepository.getOne(userId), restaurant);
         vote.setId(voteId);
-        return save(vote);
+        return repository.save(vote);
     }
 }
